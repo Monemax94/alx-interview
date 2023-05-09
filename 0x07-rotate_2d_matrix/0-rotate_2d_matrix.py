e2fedf61ced44e16adcf539e0b518a7ba764f970
Monemax94 <mondayodoh94gmail.com>
2023-05-09 09:43:59 +0100

    correction

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,12 +8,12 @@ def rotate_2d_matrix(matrix):
     """
     # get the length of the matrix
     n = len(matrix)
-    
+
     # transpose the matrix to swap rows and columns
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     # reverse each row to rotate the matrix clockwise
     for i in range(n):
         matrix[i] = matrix[i][::-1]
